3975360d969e3a1d39b7c69d7de8340bf2b54080
waltercoan <walter.coan@gmail.com>
2016-05-23 07:58:14 -0300

    Aula 20-05-2016 Exercícios blocos de repetição

---
+++ notas.py
@@ -0,0 +1,9 @@
+__author__ = 'Walter'
+cont = 0
+while cont < 6:
+    print("Aluno: ", cont+1)
+    print("Digite a primeira nota")
+    nota1 = float(input())
+    print("Digite a segunda nota")
+    nota2 = float(input())
+    cont = cont + 1
