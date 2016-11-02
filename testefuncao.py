d27904a85eb24d64335a244029d3ba5257694cc4
waltercoan <walter.coan@gmail.com>
2016-11-02 21:52:55 -0200

    Aula de introdução a funções.

---
+++ testefuncao.py
@@ -0,0 +1,31 @@
+__author__ = 'Walter'
+import random
+def musica():
+    print("Por que voce nao passa la...")
+#parametro de entrada
+def soma(num1, num2):
+    resultado = 0
+    resultado = num1 + num2
+    print("dentro", resultado)
+    return resultado
+
+def geraproxpedradobingo():
+    return random.randint(0,25)
+
+def div(num1,num2):
+    res = 0
+    res = num1 / num2
+    return res
+
+
+print(div(10,3))
+
+#print(geraproxpedradobingo())
+
+#pega = soma()
+#print("fora", pega)
+#pega = soma()
+#print("fora", pega)
+
+musica()
+
